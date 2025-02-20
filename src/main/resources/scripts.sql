73bd818ff8acd8ac9963b4c7f22a28225aeb6993
Mahesh Kappa <maheshraju579@gmail.com>
2025-02-20 16:58:05 -0500

    added temp sql file

---
+++ src/main/resources/scripts.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE shopping.t_user_details (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    city VARCHAR(100),
+    state VARCHAR(100),
+    zip_code VARCHAR(10),
+    phone_number varchar(10)
+);
+
+CREATE TABLE shopping.t_user_details (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    city VARCHAR(100),
+    state VARCHAR(100),
+    zip_code VARCHAR(10),
+    phone_number varchar(10)
+);
